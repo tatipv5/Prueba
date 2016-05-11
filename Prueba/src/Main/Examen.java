78d1e8cfb5284c0e4f2017833c2f7dfbee9627d8
tati <tati@tati-Inspiron-5545>
2016-05-10 18:00:36 -0600

    main

---
+++ Prueba/src/Main/Examen.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Vista.VentanaPrincipal;
+
+
+
+/**
+ *
+ * @author tati
+ */
+public class Examen {
+    /**
+     * @param args the command line arguments
+     */    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+       VentanaPrincipal v=new VentanaPrincipal();
+       v.setVisible(true);
+    }
+    
+}
